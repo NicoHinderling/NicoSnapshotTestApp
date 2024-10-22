320928e545eeff0f9ffda904687377a9e4db491c
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2024-10-22 11:02:29 -0700

    then make it green

--- NicoSnapshotTestApp/NicoView.swift
+++ NicoSnapshotTestApp/NicoView.swift
@@ -11,7 +11,7 @@ struct NicoView: View {
     var body: some View {
         VStack {
             Text("NicoView")
-            .foregroundStyle(.gray)
+            .foregroundStyle(.green)
         }
         .padding()
         .background(Color.blue)
